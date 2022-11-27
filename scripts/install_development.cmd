5499f87853777401312e4eba2419b45b673cab4f
basileazh <basile.elazhari@gmail.com>
2022-11-27 23:15:38 +0100

    install_development_wsl.sh: script for WSL python environment setup for developers, with tables3.6.1 wheel.

---
+++ scripts/install_development.cmd
@@ -0,0 +1,6 @@
+:: ------- DEPENDENCIES -------
+pip install dependencies_wheels/tables-3.6.1-cp38-cp38-manylinux1_x86_64.whl
+pip install -r src/requirements.txt
+
+:: ------- PRE-COMMIT -------
+pre-commit install
