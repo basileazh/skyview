0bb74f55ed9dd5ba67e07f6806dcde3af73d5314
basileazh <basile.elazhari@gmail.com>
2022-11-28 03:22:49 +0100

    data_visualization : separated volume and price plots, and plot only close price for prices

--- src/skyview/modules/data_visualization/data_visualization.py
+++ src/skyview/modules/data_visualization/data_visualization.py
@@ -1,19 +1,22 @@
 # for import config file
 import os
+from datetime import datetime
 
-import pandas
 import pandas as pd
 
-from skyview.modules.utils.utils import filter_df_columns_tickers
+# Config
+from skyview.modules.utils.utils import filter_df_columns_tickers, load_config
 
+config = load_config()
 DIR = os.path.dirname(__file__)
 
 
-def visualize_time_series_lines_by_type(df: pd.DataFrame, parameters) -> dict:
+def visualize_time_series_by_type(df: pd.DataFrame, parameters) -> dict:
     """
-    Creates a line plot with all columns in df
-    One line plot is created by type of ticker, as described in
-    src/skyview/modules/data_visualization/tickers_data.csv
+    Creates a plot with all columns in df
+    One plot is created by type of ticker, as described in
+    src/skyview/modules/data_visualization/tickers_data.csv,
+    and volume is plotted separate from prices
     :param df: df to plot
     :param parameters: In conf/base/catalog:
     Defaults to {"figsize": (30, 20)}
@@ -24,20 +27,52 @@ def visualize_time_series_lines_by_type(df: pd.DataFrame, parameters) -> dict:
     unique_df_types = tickers_df["type"].unique()
     plots_df = {}
     for ticker_type in unique_df_types:
+        # Filter on columns who belong to ticker_type
         print(f"Making df for plot for type {ticker_type}")
         type_ticker_cols = tickers_df.loc[
             tickers_df["type"] == ticker_type, "ticker"
         ].unique()
         print(f"{type_ticker_cols = }")
+
         # Melt to 3 columns : Date, ticker, value
-        plots_df[ticker_type] = _melt_df_for_plotly(
-            filter_df_columns_tickers(df, type_ticker_cols)
-        )
+        df_plot = _melt_df_for_plotly(filter_df_columns_tickers(df, type_ticker_cols))
+
         # Resample as in data_processing pipeline config
-        plots_df[ticker_type] = _resample_plotly_df(
-            plots_df[ticker_type], parameters["time_freq"]
+        df_plot = _resample_plotly_df(df_plot, parameters["time_freq"])
+
+        # Filter data between start_date_plot and end_date_plot
+        df_plot = _filter_date_plotly_df(
+            df_plot, parameters["start_date_plot"], parameters["end_date_plot"]
         )
+
+        # Filter price timings as inputed in price_timing parameter
+        # between close, open, high, low
+        df_plot = _filter_price_timing_plotly_df(df_plot, parameters["price_timing"])
+
+        # Separate proces and volume
+        plots_df[f"{ticker_type}_price"] = df_plot.loc[
+            ~df_plot["ticker"].str.startswith(parameters["volume_columns_str"])
+        ]
+        plots_df[f"{ticker_type}_volume"] = df_plot.loc[
+            df_plot["ticker"].str.startswith(parameters["volume_columns_str"])
+        ]
     return plots_df
+
+
+def _melt_df_for_plotly(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Creates a dataframe with only 3 columns :
+    Date, ticker, value
+    Input dataframe should have Date as index,
+    time series as columns
+    :param df: df to plot
+    :return: pd.DataFrame
+    """
+    # Melt data frame to obtain only 3 columns :
+    # Date, ticker, value
+    df = df.reset_index().melt(id_vars=["Date"], var_name="ticker", value_name="value")
+
+    return df
 
 
 def _resample_plotly_df(df: pd.DataFrame, freq: str = "w-MON") -> pd.DataFrame:
@@ -64,18 +99,53 @@ def _resample_plotly_df(df: pd.DataFrame, freq: str = "w-MON") -> pd.DataFrame:
     return df
 
 
-def _melt_df_for_plotly(df: pd.DataFrame) -> pandas.DataFrame:
+def _filter_date_plotly_df(
+    df: pd.DataFrame, start_date_plot: str = "2018-01-01", end_date_plot: str = "today"
+):
     """
-    Creates a dataframe with only 3 columns :
-    Date, ticker, value
-    Input dataframe should have Date as index,
-    time series as columns
-    :param df: df to plot
-    :return: pd.DataFrame
+    Filters a plotly_ready dataset
+    between start_date_plot and end_date_plot
+    This df is outputed by _melt_df_for_plotly
+    Args:
+        df: plotly-ready dataset to be filtered
+        start_date_plot: lower time bound, included
+        Defaults at "2018-01-01"
+        end_date_plot: upper time bound, included
+        If "today" is given, current date is used
+
+    Returns: filtered plotly-ready dataset
+
     """
-    # Melt data frame to obtain only 3 columns :
-    # Date, ticker, value
-    df = df.reset_index().melt(id_vars=["Date"], var_name="ticker", value_name="value")
+    if end_date_plot == "today":
+        end_date_plot = str(datetime.today().strftime(config["F"]))
+
+    start_date_plot = pd.to_datetime(start_date_plot)
+    end_date_plot = pd.to_datetime(end_date_plot)
+
+    df = df.loc[(df["Date"] >= start_date_plot) & (df["Date"] <= end_date_plot)]
+
+    return df
+
+
+def _filter_price_timing_plotly_df(
+    df: pd.DataFrame, price_timings: list = ["close"]
+) -> pd.DataFrame:
+    """
+    Filters tickers based on price timing criteria
+    Timing values : close, open, high, low
+    Args:
+        df: df to filter by price timing
+        price_timings: list in lose, open, high, low
+
+    Returns: pd.DataFrame filtered on price type
+
+    """
+    # List of tickers in price_timings
+    tickers_list = []
+    for price_timing in price_timings:
+        tickers_list.extend([t for t in df["ticker"] if t.startswith(price_timing)])
+
+    df = df.loc[df["ticker"].isin(tickers_list)]
 
     return df
 
